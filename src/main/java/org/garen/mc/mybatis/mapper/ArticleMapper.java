0eb00736554679cb7a424ac3f97509d0afa4a2d8
yanglimou <2531463756@qq.com>
2017-09-15 07:46:10 +0800

    修改mapper文件生成后启动报错

--- src/main/java/org/garen/mc/mybatis/mapper/ArticleMapper.java
+++ src/main/java/org/garen/mc/mybatis/mapper/ArticleMapper.java
@@ -1,11 +1,12 @@
 package org.garen.mc.mybatis.mapper;
 
+import java.io.Serializable;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
 import org.garen.mc.mybatis.domain.Article;
 import org.garen.mc.mybatis.domain.ArticleExample;
 
-public interface ArticleMapper {
+public interface ArticleMapper<T,Q,PK extends Serializable> extends CommonMapper<T, Q,PK> {
     long countByExample(ArticleExample example);
 
     int deleteByExample(ArticleExample example);
