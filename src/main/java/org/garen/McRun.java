d2d1a9b9e40de5cfea3f5e57d1c2bb1fa093fa04
liuxueliang <liuxueliang@yingu.com>
2017-10-18 17:40:41 +0800

    解决跨域问题

--- src/main/java/org/garen/McRun.java
+++ src/main/java/org/garen/McRun.java
@@ -1,8 +1,13 @@
 package org.garen;
 
+import org.garen.mc.swagger.ApiOriginFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.Filter;
 
 
 /**
@@ -11,6 +16,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("org.garen.mc.mybatis.mapper")
 public class McRun {
+
+
+    /**
+     * 配置过滤器
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean someFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(apiOriginFilter());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("paramName", "paramValue");
+        registration.setName("apiOriginFilter");
+        return registration;
+    }
+
+    /**
+     * 创建一个bean
+     * @return
+     */
+    @Bean(name = "apiOriginFilter")
+    public Filter apiOriginFilter() {
+        return new ApiOriginFilter();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(McRun.class, args);
